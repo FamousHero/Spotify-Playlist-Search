2607c5d29494fd13dc437eb6f5d63850bc401309
FamousHero <33110962+FamousHero@users.noreply.github.com>
2021-06-14 02:54:56 -0700

    started separating gui and backend

---
+++ src/Playlist-Search-GUI.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from client import Client
+def GUI():
+    pass
+
+client = Client()
+search_by_name = input("Would you like to search by name: (Yes/No)\n")
+run = True
+while(run):
+    if(search_by_name.lower() == "yes"):
+        client.search("name")
+        search_by_name = input("----------------"+
+        "\nWould you like to search again: (Yes/No)\n")
+    elif(search_by_name.lower() == "no"):
+        run = False
--- src/client.py
+++ src/client.py
@@ -1,45 +1,65 @@
 import requests
 import spotipy
-import tkinter as tk
 import webbrowser
-client_id = YOUR_CLIENT_ID
-client_secret = YOUR_CLIENT_SECRET
-redirect_uri = 'http://127.0.0.1:9090'
-scope = ["playlist-read-private", "playlist-read-collaborative","user-top-read"]
-#redirect to user login if no auth token or refresh token expired
-#Sooooooooo if a refresh token has expired then clear the cache associated
-#with it
-auth_manager = spotipy.SpotifyOAuth(client_id = client_id,
-client_secret = client_secret, redirect_uri= redirect_uri, scope= scope)
-user = spotipy.Spotify(auth_manager = auth_manager)
-playlists = user.current_user_playlists()
-amount_of_playlists_left = playlists["total"] - 50
-offset = 50
-while(amount_of_playlists_left > 0):
-    playlists_to_add = user.current_user_playlists(offset=offset)
-    for dict in playlists_to_add["items"]:
-        playlists["items"].append(dict)
-    amount_of_playlists_left -= 50
-    offset += 50
-#gives name of every playlist
-playlists_name_id = {}
-for index in range(len(playlists["items"])):
-    playlists_name_id[playlists["items"][index]["name"]] = playlists["items"][index]["id"]
-song_to_search = input("what song are you looking for: ")
-print(song_to_search + " is in playlist: ")
-playlists_with_song = []
-for name in playlists_name_id:
-    tracks = user.playlist_tracks(playlist_id = playlists_name_id[name], 
-    fields= "items(track(name))")
-    for index in range(len(tracks["items"])):
-        if song_to_search == tracks["items"][index]["track"]["name"]:
-            playlists_with_song.append(name)
-if(len(playlists_with_song) <= 0):
-    print("\tError: No playlist currently has this song")
-else:
-    for playlist in playlists_with_song:
-        print(playlist)
-#need a login and a logout
+class Client:
+    #implement gui to   1)take current song or a song name
+    #                   2)Ask for a new search
+    #                   3) allow user to logout
+    #implement in code
+    #                   1)add a way to check cache for current spotify account
+    #                   2)search with current song (by song_id)
+    #                   3) clear user tokens when they logout
+    
+    #Bugs: only checks the 1st 100 of eah playlist
+    def __init__(self):
+        pass
+    def search(self, searchType):
+        """Get the users playlists and pass it to the specified search"""
+        client_id = YOUR_CLIENT_ID
+        client_secret = YOUR_CLIENT_SECRET
+        redirect_uri = 'http://127.0.0.1:9090'
+        scope = ["playlist-read-private", "playlist-read-collaborative","user-top-read"]
+        
+        auth_manager = spotipy.SpotifyOAuth(client_id = client_id,
+        client_secret = client_secret, redirect_uri= redirect_uri, scope= scope)
+        user = spotipy.Spotify(auth_manager = auth_manager)
+        playlists = user.current_user_playlists()
+        amount_of_playlists_left = playlists["total"] - 50
+        offset = 50
+        while(amount_of_playlists_left > 0):
+            playlists_to_add = user.current_user_playlists(offset=offset)
+            for dict in playlists_to_add["items"]:
+                playlists["items"].append(dict)
+            amount_of_playlists_left -= 50
+            offset += 50
+        if searchType == "name":
+           self.search_by_name(input, playlists, user)
+        #elif searchType == "current_song":
+        #   search_by_current_song(client_id, client_secret, redirect_uri)
+        return
+    def search_by_name(self, song_name, playlists, user):
+        playlists_name_id = {}
+        for index in range(len(playlists["items"])):
+            playlists_name_id[playlists["items"][index]["name"]] = playlists["items"][index]["id"]
+        song_name = input("----------\n"+
+        "What song are you looking for: ")
+        print(song_name + " is in playlist: ")
+        playlists_with_song = []
+        for name in playlists_name_id:
+            tracks = user.playlist_tracks(playlist_id = playlists_name_id[name], 
+            fields= "items(track(name))")
+            for index in range(len(tracks["items"])):
+                if song_name == tracks["items"][index]["track"]["name"]:
+                    playlists_with_song.append(name)
+        if(len(playlists_with_song) <= 0):
+            print("\tError: No playlist currently has this song")
+        else:
+            for playlist in playlists_with_song:
+                print("\t"+playlist)
 
-#want to print the name of playlists with track in it
-#check the song by unique id since names can repeat
+
+    #gives name of every playlist
+    
+
+    #want to print the name of playlists with track in it
+    #check the song by unique id since names can repeat
