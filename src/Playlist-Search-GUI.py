1c474aa2f56312d055b300f64821b244e28fadd0
FamousHero <33110962+FamousHero@users.noreply.github.com>
2021-06-16 20:28:53 -0700

    Implemented basic UI

--- src/Playlist-Search-GUI.py
+++ src/Playlist-Search-GUI.py
@@ -1,17 +1,49 @@
 import tkinter as tk
+from tkinter.constants import END, INSERT
 from client import Client
 def GUI():
-    pass
+    client = Client()
+    root_window = tk.Tk()
+    root_window.title("GUI")
+    root_window.columnconfigure(0, weight=1)
+    root_window.columnconfigure(1, weight=1)
+    root_window.columnconfigure(2, weight=1)
 
-client = Client()
-search_by_name = input("Would you like to search by name: (Yes/No)\n")
-run = True
-while(run):
-    if(search_by_name.lower() == "yes"):
-        client.search("name")
-        search_by_name = input("----------------"+
-        "\nWould you like to search again: (Yes/No)\n")
-    elif(search_by_name.lower() == "no"):
-        run = False
-    else:
-        search_by_name = input()
+    root_window.rowconfigure(0, weight=1)
+    root_window.rowconfigure(1, weight=1)
+    root_window.rowconfigure(2, weight=1)
+    root_window.rowconfigure(3, weight=1)
+    root_window.rowconfigure(4, weight=1)
+
+    leftframe = tk.Frame(root_window, bg="pink", width=200)
+    leftframe.grid(row=0,rowspan=5, column=0, sticky="NSEW")
+    rightframe = tk.Frame(root_window, bg="pink", width=200)
+    rightframe.grid(row=0, rowspan=5, column=2, sticky="NSEW")
+    centertop = tk.Frame(root_window, width=400, height=250)
+    centertop.grid(row=0, rowspan=2, column=1, sticky="NSEW")
+    centerlabel = tk.Label(text="Hello", justify="center", bg="pink", fg="blue", font=("Lucida Calligraphy", "16"))
+    centerlabel.grid(row=0, column=1, sticky="NSEW")
+    midtop = tk.Frame(root_window, bg="pink", width= 400, height = 100)
+    midtop.grid(row=1, column=1, rowspan=2, sticky="NSEW")
+    centerbottom = tk.Frame(root_window, bg="pink", height=250)
+    centerbottom.grid(row=2, rowspan=3, column=1, sticky="NSEW")
+    songname = tk.Entry(root_window, justify="center")
+    songname.grid(row=1, column=1, sticky="EW")
+    songname.insert(0, "What song would you like to search")
+    
+
+    searchbutton =tk.Button(master=root_window, text="Search", command=lambda:display("name", songname.get()))
+    searchbutton.grid(row=2, column=1)
+
+    def display(type, songname):
+        playlists_to_display=client.search(type, songname)
+        centerlabel['text'] = playlists_to_display
+        centerlabel['font'] = ("Lucida Calligraphy", "16")
+    #frame = tk.Entry(root_window, text=30)
+    #frame.grid(row=1, column=1,stick= "EW")
+
+    root_window.mainloop()
+
+
+if __name__ == "__main__":
+    GUI()
--- src/client.py
+++ src/client.py
@@ -1,6 +1,7 @@
 import requests
 import spotipy
 import webbrowser
+import time
 class Client:
     #implement gui to   1)take current song or a song name
     #                   2)Ask for a new search
@@ -9,9 +10,16 @@ class Client:
     #                   1)add a way to check cache for current spotify account
     #                   2)search with current song (by song_id)
     #                   3) clear user tokens when they logout
+    #                   4) possible create a database storing playlists and their songs
+    #                   so that you dont have to get the info from the api every call (.2 per playlist,
+    #                                                                                  .1 per tracks)
+    #                   if done just check every call that the total playlists == db using offset=total(.2s)
+    #                   if it doesnt you will have the missing playlists starting at offset and 
+    #                   can reuse code to build db
+    #                   do the same for tracks
     def __init__(self):
         pass
-    def search(self, searchType):
+    def search(self, searchType, name=None):
         """Get the users playlists and pass it to the specified search"""
         client_id = YOUR_CLIENT_ID
         client_secret = YOUR_CLIENT_SECRET
@@ -22,36 +30,41 @@ class Client:
         client_secret = client_secret, redirect_uri= redirect_uri, scope= scope)
         user = spotipy.Spotify(auth_manager = auth_manager)
         playlists = user.current_user_playlists()
-        amount_of_playlists_left = playlists["total"] 
-        self.get_total_info(user, playlists, "playlist", amount_of_playlists_left)        
+        amount_of_playlists = playlists["total"] 
+        self.get_total_info(user, playlists, "playlist", amount_of_playlists)        
         if searchType == "name":
-           self.search_by_name(input, playlists, user)
+           return str(self.search_by_name(name, playlists, user))
+        
         #elif searchType == "current_song":
         #   search_by_current_song(client_id, client_secret, redirect_uri)
         return
     def search_by_name(self, song_name, playlists, user):
+        returnstring = song_name + " is in:\n"
         playlists_name_id = {}
         for index in range(len(playlists["items"])):
             playlists_name_id[playlists["items"][index]["name"]] = playlists["items"][index]["id"]
-        song_name = input("----------\n"+
-        "What song are you looking for: ")
-        print(song_name + " is in playlist: ")
         playlists_with_song = []
+        search_timer = time.time()
+        #self.search_song_in_playlists()
         for name in playlists_name_id:
+            track_time = time.time()
             tracks = user.playlist_tracks(playlist_id = playlists_name_id[name], 
             fields= "items(track(name)), total")
             self.get_total_info(user, tracks, "tracks", tracks["total"], playlists_name_id[name])
+            mid_index = tracks["total"]
             for index in range(tracks["total"]):
                 if tracks["items"][index]["track"] != None:
                     if song_name == tracks["items"][index]["track"]["name"]:
-                        playlists_with_song.append(name)
-        if(len(playlists_with_song) <= 0):
-            print("\tError: No playlist currently has this song")
+                        returnstring += name+"\n"
+        if(returnstring == song_name + " is in:\n"):
+            return "\tError: No playlist currently has this song"
         else:
-            for playlist in playlists_with_song:
-                print("\t"+playlist)
+            #for playlist in playlists_with_song:
+            #    print("\t"+playlist)
+            #print("search_by_name() time: %s" % (time.time()-search_timer))
+            return returnstring
 
-    def get_total_info(sef, user, container, type, total, playlist_id = None):
+    def get_total_info(self, user, container, type, total, playlist_id = None):
         if type == "playlist":
             offset = 50
             while(total > offset):
@@ -66,3 +79,6 @@ class Client:
                 container["items"].extend(tracks_to_add["items"])
                 offset += 100
         return
+
+    def search_song_in_playlists(self, song_name, tracks):
+        pass
